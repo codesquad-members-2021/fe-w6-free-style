9307b746ce609298f111e5673ab157dccc970027
adelakim <diddnjs02@likelion.org>
2021-03-14 23:33:30 +0900

    commit

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const apiRouter = express.Router();
 
+const question = ["1. 나는 여러 친구들과 많이 사귀는 편이다", "2. 나는 몇 명의 친구와 깊이 사귀는 편이다."];
+
 const responseBody = (question) => {
   return {
     version: "2.0",
@@ -8,7 +10,7 @@ const responseBody = (question) => {
       outputs: [
         {
           simpleText: {
-            text: `${question[0]} ${question[1]}`,
+            text: `${question[0]}\n${question[1]}`,
           },
         },
       ],
@@ -32,33 +34,8 @@ const responseBody = (question) => {
 
 apiRouter.post("/", function (req, res) {
   console.log(req.body);
-  const responseBody = {
-    version: "2.0",
-    template: {
-      outputs: [
-        {
-          simpleText: {
-            text: `1. 나는 여러 친구들과 많이 사귀는 편이다\n2. 나는 몇 명의 친구와 깊이 사귀는 편이다.`,
-          },
-        },
-      ],
-      quickReplies: [
-        {
-          messageText: "1번",
-          action: "block",
-          blockId: "604df51fb908ae1e731f0141",
-          label: "question1_1",
-        },
-        {
-          messageText: "2번",
-          action: "block",
-          blockId: "604df546048a962ecd896158",
-          label: "question1_2",
-        },
-      ],
-    },
-  };
-  res.status(200).json(responseBody);
+  const result = responseBody(question);
+  res.status(200).json(result);
 });
 
 module.exports = apiRouter;
