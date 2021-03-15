114a292524ce423b027889230ef8b75e4934ebc9
Juddroid <nobllizdc@gamil.com>
2021-03-16 04:33:58 +0900

    [Add] Title Tab 메뉴 구성

--- raccoon-corona-live/src/components/MainLayout/LayoutTitle/LayoutTitle.js
+++ raccoon-corona-live/src/components/MainLayout/LayoutTitle/LayoutTitle.js
@@ -4,7 +4,7 @@ import TitleTab from './TitleTab';
 
 function LayoutTitle() {
   return (
-    <div className="Layout__Title flex flex-row">
+    <div className="Layout__Title flex flex-column">
       <TitleMenu />
       <TitleTab />
     </div>
--- raccoon-corona-live/src/components/MainLayout/LayoutTitle/TitleTab.js
+++ raccoon-corona-live/src/components/MainLayout/LayoutTitle/TitleTab.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import TabMenu from './TabMenu';
 
 function TitleTab() {
-  return <div>tab</div>;
+  const tabMenu = ['국내', '세계', '백신', '거리두기'];
+
+  return (
+    <div className="Layout__Title--Tab flex-row flex Tab__Animation">
+      <TabMenu menu={tabMenu} />
+      <TabMenu menu={tabMenu} />
+      <TabMenu menu={tabMenu} />
+      <TabMenu menu={tabMenu} />
+    </div>
+  );
 }
 
 export default TitleTab;
