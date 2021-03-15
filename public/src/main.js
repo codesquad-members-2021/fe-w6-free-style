f8a86dbf06ec8240c2f49cde8a85bf9c22a8dd68
adelakim <diddnjs02@likelion.org>
2021-03-15 21:46:37 +0900

    [Add] response url to client, including the test's result
    - 결과를 쿼리스트링으로 보내는 부분 구현
    - 클라이언트에서 받기 완료

--- public/src/main.js
+++ public/src/main.js
@@ -1,4 +1,10 @@
-const url = location.search;
-console.log(url);
+import { _ } from "./utils.js";
 
-fetch(url).then(console.log);
+const request = async (url) => {
+  const data = await fetch(url);
+  return data;
+};
+
+const { types, scores } = request(_.search());
+const scoreArr = scores.split("");
+console.log(types, scoreArr);
