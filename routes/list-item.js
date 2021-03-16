bcf81c9047f5ffc5524fb1f23f9cf265d49fd290
eve712 <62237639+eve712@users.noreply.github.com>
2021-03-16 18:34:34 +0900

    Add: 데이터 보내주는 라우팅 작성

--- routes/list-item.js
+++ routes/list-item.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const items = require('../data/homeContents.json');
 
 router.get('/', (req, res) => {
-
+    const i = req.query.index;
+    const item = items.contents[i].eventContent;
+    res.send(item);
 });
 
 module.exports = router;
