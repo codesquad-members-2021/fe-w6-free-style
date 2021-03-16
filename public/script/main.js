c856afad2a2bd41289aef51d0c5fbac9bc54e021
eve712 <62237639+eve712@users.noreply.github.com>
2021-03-16 18:33:32 +0900

    Add: DOM 참조 요소 파일 추가, 세팅 객체 추가

--- public/script/main.js
+++ public/script/main.js
@@ -1,20 +1,11 @@
-import Carousel from './carousel.js';
-import MoreData from './moreData.js';
+import Carousel from './carousel';
+import MoreData from './moreData';
+import ref from './ref'
 
-const ref = {
-    bannerRef: {
-        prevBtn: document.querySelector('.btn_prev'),
-        nextBtn: document.querySelector('.btn_next'),
-        slideWrap: document.querySelector('.slide__wrap'),
-        pagingList: document.querySelectorAll('.ico_paging'),
-        pagingIcons: document.querySelectorAll('.num_page'),
-        slideTime: 400,
-        slideNum: 3
-    },
-    moreData: {
-        moreBtn: document.querySelector('.showMore')
-    }
+const setting = {
+    URL: '',
+    loadingNum: 5
 }
-console.log(banner);
+
 const carousel = new Carousel(ref);
-const moreData = new MoreData(ref); // URL 인자로 넘겨줘야 함
+const moreData = new MoreData(ref, setting);
---
+++ public/script/ref.js
@@ -0,0 +1,15 @@
+export default ref = {
+    bannerRef: {
+        prevBtn: document.querySelector('.btn_prev'),
+        nextBtn: document.querySelector('.btn_next'),
+        slideWrap: document.querySelector('.slide__wrap'),
+        pagingList: document.querySelectorAll('.ico_paging'),
+        pagingIcons: document.querySelectorAll('.num_page'),
+        slideTime: 400,
+        slideNum: 3
+    },
+    moreData: {
+        moreBtn: document.querySelector('.showMore'),
+        listWrap: document.querySelector('.banner__list')
+    }
+}
