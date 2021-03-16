36ff5ae6744986f23a85335a45eb708d2caeded1
Juddroid <raccoon@Juddroids-MacBook-Pro.local>
2021-03-16 18:54:37 +0900

    [Add] Section Map & Region Tab

--- raccoon-corona-live/src/components/MainLayout/SectionChart/SectionChart.js
+++ raccoon-corona-live/src/components/MainLayout/SectionChart/SectionChart.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import ChartBox from './ChartBox';
 
 function SectionChart() {
-    return <div className="Layout__Section"></div>
+    return (
+    <div className="Section__Chart">
+        <ChartBox />
+    </div>
+    )
 }
 
 export default SectionChart;
--- raccoon-corona-live/src/components/MainLayout/SectionTodayPerson/SectionTodayBottom.js
+++ raccoon-corona-live/src/components/MainLayout/SectionTodayPerson/SectionTodayBottom.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import TodayBottomBox from './TodayBottomBox';
 
 function SectionTodayBottom() {
-    return <div className="Section__Today__Bottom section__stretch section__animation flex-column flex"></div>
+    return (
+    <div className="Section__Today__Bottom section__stretch section__animation flex-column flex">
+        <TodayBottomBox />
+    </div>
+    )
 }
 
 export default SectionTodayBottom;
