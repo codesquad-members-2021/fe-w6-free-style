a2796604209931ef2ba8386f66eda67bb5cc8f0d
17-sss <xzxking17@gmail.com>
2021-03-17 14:47:39 +0900

    [Add] connect-flash module (Temporary apply) - Used to show a errorMessage to the client

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express');
-const ejs = require('ejs');
 const path = require('path');
+const session = require('express-session');
+const flash = require('connect-flash');
 const dotenv = require('dotenv');
-const { sequelize } = require("./models");
+const ejs = require('ejs');
+
+dotenv.config();
 
 const vendorsRouter = require('./routes/vendors.js');
 const indexRouter = require('./routes/index.js');
 const authRouter = require('./routes/auth.js');
 const todoRouter = require('./routes/todo.js');
+const { sequelize } = require("./models");
 
 const app = express();
 sequelize.sync(); 
 
-dotenv.config();
+app.locals.pretty = true;
 
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'html');
@@ -22,6 +26,12 @@ app.set('port', process.env.SERVER_PORT || 4000);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// connect-flash 모듈을 사용하기 위해 express-session 모듈 사용
+app.use(session({
+    secret: 'cookiesecret_temp',    
+}));
+app.use(flash());
 
 app.use('/vendors', vendorsRouter);
 app.use('/', indexRouter); 
