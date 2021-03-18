362f1f1fd3c9b044c11c5f400cec7e91586c65d8
eve712 <62237639+eve712@users.noreply.github.com>
2021-03-18 17:08:36 +0900

    Fix: 더보기 아이템 추가를 위해 아이템 리스트 ul태그 높이 없앰

--- public/script/moreData.js
+++ public/script/moreData.js
@@ -1,17 +1,36 @@
 export default class MoreData {
-    constructor({moreData}, URL) {
+    constructor({moreData}, {URL, loadingNum}) {
         this.moreBtn = moreData.moreBtn;
+        this.listWrap = moreData.listWrap;
         this.URL = URL;
+        this.loadingNum = loadingNum;
+        this.currItems = 4;
         this.setEvent();
     }
     setEvent() {
         this.moreBtn.addEventListener('click', );
     }
-    renderItems() {
-
+    loadItems() {
+        for(let i = 0; i < this.loadingNum; i++) {
+            this.fetchData(this.currItems + i);
+        }
     }
     fetchData(idx) {
         fetch(`${this.URL}/item-list?index=${idx}`)
-            .then()
+            .then(this.renderItems)
+    }
+    renderItems(items) {
+        const data = [items.imgurl, items.title, items.subtitle]
+        const temp = this.getTemplate(data);
+        this.listWrap.appendChild(temp);
+    }
+    getTemplate([imgurl, title, subtitle]) {
+        const temp = `<li class="banner__item list__item"><a href="#">
+            <img src= ${imgurl} alt= ${title}>
+            <span class="list__title">${title}</span>
+            <span class="list__des">${subtitle}</span>
+            <span class="list__theme">테마</span>
+        </a></li>`
+        return temp;
     }
 }
