df37cd5babe850b4dca4ccf8dfe0699ebf157c32
Jaeuk-YOO <ryou9305@naver.com>
2021-03-18 09:43:14 +0900

    [add] API 배경 불러오는 함수 추가

---
+++ frontend/src/util/API.js
@@ -0,0 +1,19 @@
+const PIXABAY_ENDPOINT = "https://pixabay.com/api/";
+
+const API = {
+  get: {
+    randomImage: () => {
+      const idx = parseInt(Math.random()*10-1);
+      const queries = ["flower", "baby", "human", "color", "game", "food", "travel", "toy", "sky", "coffee"];
+      const result = fetch(`${PIXABAY_ENDPOINT}/?key=${process.env.PIXABAY_API_KEY}&q=${queries[idx]}&orientation=horizontal&image_type=photo`)
+      .then(async (response) => {
+          return await response.json();
+        }
+      )
+      return result;
+    }
+  },
+  post: {}
+}
+
+export default API;
