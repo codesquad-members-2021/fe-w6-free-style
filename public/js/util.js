91b0193ac4b71aad761459f8a7f3575e30b999ae
swing <69034766+swing-park@users.noreply.github.com>
2021-03-19 01:31:33 +0900

    [swing] codeRunner step1 (#47)
    * Add : initial environment
    
    * Add : codeRunner

---
+++ public/js/util.js
@@ -0,0 +1,23 @@
+const _ = {
+    $: (selector, base = document) => base.querySelector(selector),
+    $All: (selector, base = document) => base.querySelectorAll(selector),
+    addClass: (className, target) => target.classList.add(className),
+    removeClass: (className, target) => target.classList.remove(className),
+    on: (el, event, fn) => el.addEventListener(event, fn),
+    match: (el, regExp) => el.match(regExp),
+    replace: (el, regExp, replace) => el.replace(regExp, replace)
+};
+
+const delay = (fn, ms) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(fn()), ms);
+    })
+};
+
+const setStyle = ({ el, styleRef }) => {
+    el.style.transition = styleRef.transition;
+    el.style.transform = styleRef.transform;
+    el.style.opacity = styleRef.opacity;
+}
+
+export { _, delay, setStyle }
