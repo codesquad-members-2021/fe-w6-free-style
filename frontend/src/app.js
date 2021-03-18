3f69d2cb60490722a60bfc287917378d5129dce3
Jaeuk-YOO <ryou9305@naver.com>
2021-03-18 09:46:49 +0900

    [add] 기초 컴포넌트 설정 및 스타일링 추가

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,7 +1,28 @@
+import BackgroundContainer from "./components/Background/BackgroundContainer.js"
+import CenterWidgetComposition from "./components/CenterWidget/CenterWidgetComposition.js";
+
+import _$ from "./util/MyQuerySelector.js";
+import "./app.scss";
+
 class App {
   constructor({ $target }) {
-    this.$target = $target;
-    $target.appendChild(document.createElement("section"));
+    
+    const $backgroundSection = document.createElement("section");
+    $backgroundSection.className = "background-section";
+    $target.appendChild($backgroundSection);
+
+    const $centerWidgetSection = document.createElement("section");
+    $centerWidgetSection.className = "center-widget-section";
+    $target.appendChild($centerWidgetSection);
+
+    this.components = {
+      background: new BackgroundContainer({
+        $target: $backgroundSection,
+      }),
+      centerWidget: new CenterWidgetComposition({
+        $target: $centerWidgetSection
+      })
+    }
   }
 }
 
