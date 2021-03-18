01505f1314d173b7185d912ff686726c71ffa0b5
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-03-19 00:25:01 +0900

    Minor Fix : 줄 간격 등 마이너 픽스

--- MARKDOWN-BLOG/server.js
+++ MARKDOWN-BLOG/server.js
@@ -6,23 +6,28 @@ const loginRouter = require('./routes/logins.js');
 const methodOverride = require('method-override');
 const app = express();
 
-
 mongoose
-.connect('mongodb://localhost/glogTest', {useNewUrlParser : true, useUnifiedTopology: true, useCreateIndex : true})
-.then(() => console.log( 'Database Connected'))
-.catch(err => console.log(err));
+    .connect('mongodb://localhost/glogTest', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 
 app.set('view engine', 'ejs')
 app.use(express.static("public"))
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
-    const articles = await Article.find().sort({createdAt: 'desc'})
-    res.render("articles/index", { articles: articles});
+    const articles = await Article.find().sort({ createdAt: 'desc' })
+    res.render("articles/index", { articles: articles });
 })
+
+
 
 app.use('/login', loginRouter);
 app.use('/articles', articleRouter);
