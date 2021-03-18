ff71856525c27f646024da4d4bbf0009065cf7ae
Hannah Park <ha3158987@gmail.com>
2021-03-18 22:56:47 +0900

    Feat: add display module & make product template

--- itda-sales-platform/public/js/store.js
+++ itda-sales-platform/public/js/store.js
@@ -1,5 +1,7 @@
+import { _ } from './utill.js';
 
-let store;
+let store = _.getStorageItem('store');
+
 const setupStore = (products) => {
     store = products.map((product) => {
         //destructuring으로 필요한 것들만 가져오기
@@ -7,6 +9,9 @@ const setupStore = (products) => {
         const image = img[0].thumbnails.large.url;
         return { id, featured, name, price, company, colors, image };
     });
+    //localstorage에 store배열 저장
+    _.setStorageItem('store', store);
 }
+
 
 export { store, setupStore };
