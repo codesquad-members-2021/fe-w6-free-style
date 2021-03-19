d1498e4f0c03c23985b455694c61efc18655ae63
Juddroid <nobllizdc@gamil.com>
2021-03-19 23:30:14 +0900

    [Add] toast UI 차트 적용해서 데이터 넣기까지 해봤다...

--- raccoon-corona-live/src/components/MainLayout/SectionChart/Chart.js
+++ raccoon-corona-live/src/components/MainLayout/SectionChart/Chart.js
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { LineChart } from '@toast-ui/react-chart';
 
 function Chart() {
-  return <div className="Chart"></div>;
+  const data = {
+    categories: ['0', '0', '0', '0', '0', '0'],
+    series: [
+      {
+        name: 'today',
+        data: [0, 0, 0, 0, 0, 0],
+      },
+      {
+        name: 'yesterday',
+        data: [0, 0, 0, 0, 0, 0],
+      },
+    ],
+  };
+
+  const options = {
+    chart: {
+      width: 450,
+      height: 400,
+      title: 'Corona Live',
+      format: '1,000',
+    },
+    yAxis: {
+      title: 'count',
+    },
+    xAxis: {
+      title: 'time',
+    },
+    series: {
+      showLabel: true,
+    },
+  };
+
+  const [updateData, setUpdateData] = useState(data);
+
+  async function fetchData() {
+    const request = `/domestic-init.json`;
+    const response = await axios.get(request);
+    const data = response.data.timeseries;
+    console.log(data.today);
+
+    const timeArr = (key) => Object.keys(key);
+    const countArr = (value) => Object.values(value);
+
+    setUpdateData({
+      categories: timeArr(data.today),
+      series: [
+        {
+          name: 'today',
+          data: countArr(data.today),
+        },
+        {
+          name: 'yesterday',
+          data: countArr(data.yesterday),
+        },
+      ],
+    });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="Chart">
+      <LineChart data={updateData} options={options} />
+    </div>
+  );
 }
 
 export default Chart;
