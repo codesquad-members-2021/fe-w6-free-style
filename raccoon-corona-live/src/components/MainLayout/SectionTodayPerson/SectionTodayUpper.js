a015656271bd405708311e90f7c44d93bcda1911
Juddroid <nobllizdc@gamil.com>
2021-03-19 16:37:05 +0900

    [Fix] 해결된게 아니었따... / 다시 공부해야된다. 뭔가 단단히 오해하고 있다.

--- raccoon-corona-live/src/components/MainLayout/SectionTodayPerson/SectionTodayUpper.js
+++ raccoon-corona-live/src/components/MainLayout/SectionTodayPerson/SectionTodayUpper.js
@@ -10,49 +10,45 @@ function SectionTodayUpper() {
       number: [0, 0],
     },
   ];
-  const statsLive2 = [
-    {
-      text: ['2주전', '1달전'],
-      number: [0, 0],
-    },
-  ];
+  // const statsLive2 = [
+  //   {
+  //     text: ['2주전', '1달전'],
+  //     number: [0, 0],
+  //   },
+  // ];
 
   const [stats1, setStats1] = useState(statsLive1);
-  const [stats2, setStats2] = useState(statsLive2);
+  // const [stats2, setStats2] = useState(statsLive2);
 
   async function fetchData() {
     const request = `/domestic-init.json`;
     const response = await axios.get(request);
     const data = response.data.statsLive;
-    return data;
-  }
 
-  async function getData() {
-    const response = await fetchData();
-
-    setStats1({
-      ...stats1,
-      text: ['어제', '1주전'],
-      number: [response.yesterday, response.weekAgo],
-    });
-    setStats2({
-      ...stats2,
-      text: ['2주전', '1달전'],
-      number: [response.twoWeeksAgo, response.monthAgo],
-    });
-    console.log(stats1, stats2);
+    setStats1([
+      {
+        ...statsLive1,
+        [stats1.number]: [data.yesterday, data.weekAgo],
+      },
+    ]);
+    debugger;
+    console.log(stats1);
+    // setStats2([
+    //   {
+    //     [stats2.number]: [data.twoWeeksAgo, data.monthAgo],
+    //   },
+    // ]);
   }
 
   useEffect(() => {
-    console.log('getData');
-    getData();
+    fetchData();
   }, []);
 
   return (
     <div className="Section__Today__Upper flex-row flex">
       <TodayLeft />
-      <TodayRight text={stats1.text} number={stats1.number} />
-      <TodayRight text={stats2.text} number={stats2.number} />
+      <TodayRight text={stats1[0].text} number={stats1[0].number} />
+      {/* <TodayRight text={stats2[0].text} number={stats2[0].number} /> */}
     </div>
   );
 }
