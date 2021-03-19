fa44064515f40f0c39a9556e5e624dd4d4a2d370
Juddroid <nobllizdc@gamil.com>
2021-03-19 22:22:55 +0900

    [Fix] 변수 약간 수정

--- raccoon-corona-live/src/components/MainLayout/SectionTodayPerson/SectionTodayUpper.js
+++ raccoon-corona-live/src/components/MainLayout/SectionTodayPerson/SectionTodayUpper.js
@@ -4,38 +4,37 @@ import TodayRight from './TodayRight';
 import axios from 'axios';
 
 function SectionTodayUpper() {
-  const statsLive1 = {
-      text: ['어제', '1주전'],
-      number: [0, 0],
-    };
-  const statsLive2 = {
-      text2: ['2주전', '1달전'],
-      number2: [0, 0],
-    };
+  const todayCenterData = {
+    textCenter: ['어제', '1주전'],
+    number: [0, 0],
+  };
+  const todayRightData = {
+    textRight: ['2주전', '1달전'],
+    number: [0, 0],
+  };
 
-  const [stats1, setStats1] = useState(statsLive1);
-  const [stats2, setStats2] = useState(statsLive2);
+  const [todoayCenter, setTodayCenter] = useState(todayCenterData);
+  const [todoayRight, setTodayRight] = useState(todayRightData);
 
-  const { text, number } = stats1;
-  const { text2, number2 } = stats2;
+  const { textCenter } = todoayCenter;
+  const { textRight } = todoayRight;
 
   async function fetchData() {
     const request = `/domestic-init.json`;
     const response = await axios.get(request);
     const data = response.data.statsLive;
-    console.log(data)
-    debugger;
-    const update = {
-      text,
-      number: [data.yesterday, data.weekAgo]
-    }
-    const update2 = {
-      text2,
-      number2: [data.twoWeeksAgo, data.monthAgo]
-    }
-  
-    setStats1(update);
-    setStats2(update2);
+
+    const updateTodayCenter = {
+      textCenter,
+      number: [data.yesterday, data.weekAgo],
+    };
+    const updateTodayRight = {
+      textRight,
+      number: [data.twoWeeksAgo, data.monthAgo],
+    };
+
+    setTodayCenter(updateTodayCenter);
+    setTodayRight(updateTodayRight);
   }
 
   useEffect(() => {
@@ -45,8 +44,8 @@ function SectionTodayUpper() {
   return (
     <div className="Section__Today__Upper flex-row flex">
       <TodayLeft />
-      <TodayRight text={stats1.text} number={stats1.number} />
-      <TodayRight text={stats2.text2} number={stats2.number2} />
+      <TodayRight text={todoayCenter.textCenter} number={todoayCenter.number} />
+      <TodayRight text={todoayRight.textRight} number={todoayRight.number} />
     </div>
   );
 }
